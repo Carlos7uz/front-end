42e81a1f5ec68f1ef20ea8753000ab11f3e60bf7
CarlosLuz <carlosluz12892@gmail.com>
2025-06-02 17:00:18 -0300

    refactor: remove AppComponent tests and implement HomeComponent with new layout
    feat: update app configuration to include NgxSpinner and animations
    
    feat: add routing for Home and Login components
    
    style: enhance index.html with Google Fonts and Material Icons
    
    style: apply global styles and set font family to Roboto
    
    feat: create HomeComponent with landing page structure and styles
    
    feat: implement LoginComponent with reactive form and loading spinner
    
    style: design LoginComponent layout with responsive styles

---
+++ src/app/features/login/login.component.ts
@@ -0,0 +1,60 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatInputModule,
+    NgxSpinnerModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  private readonly router = inject(Router);
+  private readonly fb = inject(FormBuilder);
+  private readonly snackBar = inject(MatSnackBar);
+  private readonly spinner = inject(NgxSpinnerService);
+
+
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  constructor(){
+
+  }
+
+
+  onSubmit(){
+    if (this.loginForm.valid) {
+      this.spinner.show();
+      setTimeout(() => {
+        this.router.navigate(['/home']);
+        this.spinner.hide();
+      }, 3000);
+    } else {
+      this.spinner.hide();
+      this.snackBar.open('Please fill in all fields correctly.', 'Close', {
+        duration: 3000,
+      });
+    }
+  }
+
+}
